0eefb8ea259c6c6e68663d040ae7b149fa3c3bc4
Dan Stocker <dan@kwaia.com>
2014-09-29 08:11:27 +0100

    version 0.2.5

--- README.md
+++
@@ -1,4 +0,0 @@
-Candystore
-==========
-
-*Basic widgets*
--- src/exports.js
+++
@@ -1,7 +0,0 @@
-/**
- * Library exports
- */
-/*global candystore, module */
-if (typeof module === 'object') {
-    module.exports = candystore;
-}
