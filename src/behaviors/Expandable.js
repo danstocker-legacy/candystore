c3c4201392d0aa96e8da46627ba59adde8572deb
Dan Stocker <dan@kwaia.com>
2014-11-23 15:15:49 +0000

    bases Expandable trait on BinaryStateful

--- src/behaviors/Expandable.js
+++ src/behaviors/Expandable.js
@@ -6,55 +6,75 @@ troop.postpone(candystore, 'Expandable', function () {
         self = base.extend();
 
     /**
-     * @name candystore.Expandable.create
-     * @function
-     * @returns {candystore.Expandable}
-     */
-
-    /**
+     * TODO: Add unit tests.
      * @class
      * @extends troop.Base
+     * @extends shoeshine.BinaryStateful
      * @extends shoeshine.Widget
      */
     candystore.Expandable = self
         .addConstants(/** @lends candystore.Expandable */{
+            /** @constant */
+            STATE_NAME_EXPANDABLE: 'state-expandable',
+
             /** @constants */
             EVENT_EXPAND: 'expand',
 
             /** @constants */
             EVENT_RETRACT: 'retract'
         })
+        .addPrivateMethods(/** @lends candystore.Expandable# */{
+            /** @private */
+            _updateExpandedState: function () {
+                if (this.isStateOn(this.STATE_NAME_EXPANDABLE)) {
+                    this
+                        .removeCssClass('widget-retracted')
+                        .addCssClass('widget-expanded');
+                } else {
+                    this
+                        .removeCssClass('widget-expanded')
+                        .addCssClass('widget-retracted');
+                }
+            }
+        })
         .addMethods(/** @lends candystore.Expandable# */{
             /** Call from host's init. */
             init: function () {
-                this.retractWidget();
+                // expansion is not cascading (by default)
+                this.addBinaryState(this.STATE_NAME_EXPANDABLE);
+            },
+
+            /** @ignore */
+            afterStateOn: function (stateName) {
+                if (stateName === this.STATE_NAME_EXPANDABLE) {
+                this._updateExpandedState();
+                this.triggerSync(this.EVENT_EXPAND);
+                    }
+            },
+
+            /** @ignore */
+            afterStateOff: function (stateName) {
+                if (stateName === this.STATE_NAME_EXPANDABLE) {
+                this._updateExpandedState();
+                this.triggerSync(this.EVENT_RETRACT);
+                }
             },
 
             /** @returns {candystore.Expandable} */
             expandWidget: function () {
-                this
-                    .removeCssClass('widget-retracted')
-                    .addCssClass('widget-expanded');
-
-                this.triggerSync(this.EVENT_EXPAND);
-
+                this.addBinaryStateSource(this.STATE_NAME_EXPANDABLE, 'default');
                 return this;
             },
 
             /** @returns {candystore.Expandable} */
             retractWidget: function () {
-                this
-                    .removeCssClass('widget-expanded')
-                    .addCssClass('widget-retracted');
-
-                this.triggerSync(this.EVENT_RETRACT);
-
+                this.removeBinaryStateSource(this.STATE_NAME_EXPANDABLE, 'default');
                 return this;
             },
 
             /** @returns {boolean} */
             isExpanded: function () {
-                return this.hasCssClass('widget-expanded');
+                return this.isStateOn(this.STATE_NAME_EXPANDABLE);
             }
         });
 });
